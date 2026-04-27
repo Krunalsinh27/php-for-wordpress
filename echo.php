a62bdabe3ea86ef4d94bc40b1feb2fe1d816aa29
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-04-27 11:53:06 +0530

    Add echo file

---
+++ echo.php
@@ -0,0 +1,20 @@
+<?php
+
+// incasesensitive 
+
+echo 20+20;
+echo "</br>";
+ECHO 10+10;
+ECHO "</br>";
+
+// Multiple echo in single line
+echo "Hello,"; echo "I am a developer"; echo "i am a fresher"; echo "</br>";
+
+
+// for the line break 
+echo "My name is Krunalsinh,"; 
+echo  "</br>";
+echo "I am a PHP Devloper"; 
+echo "</br>";
+echo "And also wordpress developer";
+?>
