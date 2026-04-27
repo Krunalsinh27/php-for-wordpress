cd5cb941328f5a8ef5a34682a5d8cbc5f1ed78a4
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-04-27 13:46:54 +0530

    Add variables fils

---
+++ variables.php
@@ -0,0 +1,41 @@
+<?php
+$name = "Krunalsinh";
+echo $name;
+
+echo "</br>";
+$num = 10;
+echo $num * 5;
+echo "</br>";
+echo $num * 7;
+echo "</br>";
+
+// rules of variables
+
+// 1. Case sensitive
+$name = "Krunalsinh";
+$Name = "parmar";
+$NAME = "Krunal";
+
+echo $name;
+echo "</br>";
+echo $Name;
+echo "</br>";
+echo $NAME;
+echo "</br>";
+
+// 2. Not a start with a number & special character
+// $1name = "Krunalsinh";
+// echo $1name; //error
+
+// $$name = "Raj";
+// echo $name; //error
+
+// 3. start with letter or underscore
+$_name = "Raj";
+$surname = "patel";
+
+echo $_name;
+echo "</br>";
+echo $surname;
+echo "</br>";
+?>
