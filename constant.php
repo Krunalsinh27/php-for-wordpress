8f371988bbf1e9d8ce129dc14a298adadbd2e0ba
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-04-28 17:20:55 +0530

    Add Constant file

---
+++ constant.php
@@ -0,0 +1,11 @@
+<!-- <?php
+    const name = "Krunalsinh";
+    // const name = "Ram"; //throw error
+    echo name;
+?> -->
+
+<!-- <?php
+    define(name, "Krunalsinh");
+    // define(name, "Parmar"); //give error
+    echo name;
+?> -->
