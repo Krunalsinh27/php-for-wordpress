1c65cc68e8161d44f32c4ca4b449fd46a27c339f
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-04-28 16:58:59 +0530

    Add PHP with HTML

---
+++ php-with-html.php
@@ -0,0 +1,28 @@
+<!-- <?php
+echo "<h1 style='color:green'>I am Krunalsinh</h1>";
+?>
+
+<?php
+$name = "Parmar";
+echo "<h1 style= 'color:blue'>$name</h1>";
+?>
+
+<h1 style='color:red'>
+    My Surname is, 
+    <?php
+    echo $name;
+    ?>
+
+</h1> -->
+
+<!-- dynamic -->
+ <?php
+    $h2_color= "skyblue";
+ ?>
+    <h2 style="color: <?php echo $h2_color; ?>">I am Krunalsinh</h2>
+    <h2 style="color: <?php echo $h2_color; ?>">I am Web Devloper</h2>
+
+    <?php
+        $name = "Parmar";
+        echo "<h1 style= 'color:blue'>$name</h1>";
+    ?>
