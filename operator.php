7162abc1cf5b74ff6dd2ff730d956085b1b8e9e0
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-04-29 18:55:49 +0530

    Add operator

---
+++ operator.php
@@ -0,0 +1,122 @@
+<!-- <?php
+
+// Arithmatic operator
+
+//1. Addition
+$num1 = 10;
+$num2 = 20;
+echo $num1+$num2;
+echo "</br>";
+
+//2. substraction
+$num1=15;
+$num2=10;
+echo $num1-$num2;
+echo "</br>";
+
+//3. multiplication
+$num1=15;
+$num2=10;
+echo $num1*$num2;
+echo "</br>";
+
+//4. division
+$num1=15;
+$num2=5;
+echo $num1/$num2;
+echo "</br>";
+
+//4. modulus
+$num1=15;
+$num2=10;
+echo $num1%$num2;
+echo "</br>";
+
+//5. exponentiation
+$num1=2;
+$num2=3;
+echo $num1**$num2;
+echo "</br>";
+
+?> -->
+
+<!-- <?php 
+
+// Assignment Operator
+$a = 10;
+$b = 5;
+
+$a = $b;
+echo $a;
+
+$a += $b;
+echo $a;
+
+$a -= $b;
+echo $a;
+
+$a *= $b;
+echo $a;
+
+$a /= $b;
+echo $a;
+
+$a %= $b;
+echo $a;
+
+?> -->
+
+<!-- <?php
+
+// comparison operator
+$a=5;
+$b=10;
+$c="5";
+$d=2;
+
+var_dump($a==$b);
+var_dump($a!=$b);
+
+var_dump($a===$c);
+var_dump($a!==$d);
+
+var_dump($a<>$b);
+
+var_dump($a>$b);
+var_dump($a>=$d);
+
+var_dump($a<$b);
+var_dump($a<=$c);
+
+var_dump($a<=>$d);
+
+
+?> -->
+
+<!-- <?php
+// Incremenet && Decrement
+
+$a = 10;
+
+echo $a;
+echo "</br>";
+echo "</br>";
+
+echo ++$a;
+echo "</br>";
+echo "</br>";
+
+
+echo $a++;
+echo "</br>";
+echo "</br>";
+echo $a;
+
+echo --$a;
+echo "</br>";
+echo "</br>";
+
+
+echo $a--;
+echo "</br>";
+?> -->
