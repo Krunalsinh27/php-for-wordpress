0b77f929e9e5fd2f0cb62ab0c99bb8b1113b0de1
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-04-29 18:08:43 +0530

    Add datatype

---
+++ datatype.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = "Krunalsinh";
+// echo var_dump($name);
+
+$num = 5;
+// echo var_dump($num);
+
+$numFloat = 5.14;
+// echo var_dump($numFloat);
+
+$bool = false;
+// echo var_dump($bool);
+
+$arr = ["krunal", "komal", 27];
+// echo var_dump($arr);
+
+$empty = NULL;
+// echo var_dump($empty);
+
+
+?>
