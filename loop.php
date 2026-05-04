9c8227563d9afd9a17cd94d89976626e05c2335f
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-05-04 22:35:49 +0530

    Add loop

---
+++ loop.php
@@ -0,0 +1,20 @@
+<!-- <?php
+
+$number = 15;
+// For loop 
+for($i = 0; $i<=10; $i++){
+    echo "$number * $i = " . ($number * $i) . "<br>";
+} 
+
+?> -->
+
+<?php
+//nested loop
+for($i=1; $i<=5; $i++){
+    for($j=1; $j<=$i; $j++){
+        echo "* ";
+    }
+    echo "<br>";
+}
+
+?>
