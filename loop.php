95da67ae049353d29c1d9fb5e8b6c54acd7c07a3
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-05-04 22:58:56 +0530

    add while loop in loop f

--- loop.php
+++ loop.php
@@ -8,13 +8,24 @@ for($i = 0; $i<=10; $i++){
 
 ?> -->
 
-<?php
+<!-- <?php
 //nested loop
 for($i=1; $i<=5; $i++){
     for($j=1; $j<=$i; $j++){
         echo "* ";
     }
     echo "<br>";
+}
+
+?> -->
+
+<?php
+//while loop
+
+$i = 1;
+while($i <= 10){
+    echo "The number is: $i <br>";
+    $i++;
 }
 
 ?>
