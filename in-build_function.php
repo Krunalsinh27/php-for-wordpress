84589fa099aa88475d9e64523700b87e9c83cecb
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-05-11 22:36:34 +0530

    add in-Build Funtion

---
+++ in-build_function.php
@@ -0,0 +1,31 @@
+<?php
+
+// $str = "Parmar krunalsinh";
+// echo strlen($str);
+
+// $num = 10;
+// echo var_dump($num);
+
+// echo date("d");
+// echo date("l");
+// echo date("m.d.y");
+
+// echo phpinfo(); 
+
+// $str = "Krunalsinh";
+// echo is_string($str);
+
+// $var = "abc";
+// echo is_int($var);
+
+// echo rand();
+
+// echo "before";
+// die();
+// exit();
+// echo "after";
+
+$str = "Krunalsinh parmar";
+echo substr($str, 10);
+
+?>
